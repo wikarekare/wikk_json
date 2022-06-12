8cf3414090f7dd4a6bc25a9adc7ad5b4088c4a6a
robertburrowes <r.burrowes@auckland.ac.nz>
2022-06-13 11:55:00 +1200

    rubocop'd

--- lib/boolean.rb
+++ lib/boolean.rb
@@ -2,8 +2,8 @@ class TrueClass
   # Adds formatted json convertion
   #
   # @return [String] Json for the String, which is quoted self.
-  def to_j(indent=0)
-    "  "*indent + "true"
+  def to_j(indent = 0)
+    '  ' * indent + 'true'
   end
 end
 
@@ -11,8 +11,7 @@ class FalseClass
   # Adds formatted json convertion
   #
   # @return [String] Json for the String, which is quoted self.
-  def to_j(indent=0)
-    "  "*indent + "false"
+  def to_j(indent = 0)
+    '  ' * indent + 'false'
   end
 end
-
